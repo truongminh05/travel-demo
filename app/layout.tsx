cfc88fda1899990810183060c688447edc9751c4
Truong Minh <minhhuyenkuti@gmai.com>
2025-11-19 18:14:47 +0700

    căn chỉnh tỉ lệ khung hình

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Analytics } from "@vercel/analytics/next";
